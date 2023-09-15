1847214ba07cba77e58adf7aa20991cbcafb344c
Anirudh <anirudh.venkatesh@csu.fullerton.edu>
2023-09-14 23:49:37 -0700

    Use time.process_time

--- src/analysis.py
+++ src/analysis.py
@@ -25,17 +25,21 @@ def sorting_runtime_all(input_data, algorithms):
 # measure RUNTIME of a SINGLE algorithm on a SINGLE set of data
 def sorting_runtime_one(algorithm_function, input_data):
 	unsorted_data = input_data.copy()	# Make a copy to avoid modifying the original data
-
+	"""
 	if sys.platform == "win32":
 	    # on Windows, the best timer is time.clock()
 	    default_timer = time.clock
 	else:
 	    # on most other platforms the best timer is time.time()
 	    default_timer = time.time
+	"""
 
-	start_time = default_timer()
+        #No use of default_timer
+	#default_timer=time.process_time
+
+	start_time = time.process_time()
 	sorted_data = algorithm_function(unsorted_data)  
-	end_time = default_timer()
+	end_time = time.process_time()
 
 	time_taken = end_time - start_time
 
