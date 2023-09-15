8b11c8f1429b12c63809e35ed14fab99f68254b3
anuragganji <anuragganji09@gmail.com>
2023-09-14 18:38:18 -0700

    testcases

--- TestCases/test.py
+++ TestCases/test.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.append('../')   
-from DSA.Algorithms import HeapSort
-from DSA.Algorithms import QuickSort
-from DSA.Algorithms import MergeSort
-from DSA.Algorithms import BubbleSort
-from DSA.Algorithms import BucketSort
-from DSA.Algorithms import CountingSort
-from DSA.Algorithms import RadixSort
+from DSA.src.algorithms import heap_sort
+from DSA.src.algorithms import quicksort
+from DSA.src.algorithms import merge_sort
+from DSA.src.algorithms import bubble_sort
+from DSA.src.algorithms import bucket_sort
+from DSA.src.algorithms import counting_sort
+from DSA.src.algorithms import radix_sort
 
 input_test_arrays = [[1, 12, 9, 5, 6, 10],
                     [-1, 12, 0, -5, 6, 10],
@@ -22,7 +22,7 @@ def test_quicksort():
                     [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
     print("QUICK SORT:",end=" ")
     for i in range(len(input_test_arrays)):
-        QuickSort.Sort(input_test_arrays[i], 0, len(input_test_arrays[i]) - 1)
+        quicksort.sort(input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
     print("PASSED")
       
@@ -33,7 +33,7 @@ def test_heapsort():
                     [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
     print("HEAP SORT:",end=" ")
     for i in range(len(input_test_arrays)):
-        HeapSort.Sort(input_test_arrays[i])
+        heap_sort.sort(input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
     print("PASSED")
 
@@ -43,7 +43,7 @@ def test_mergesort():
                     [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
     print("MERGE SORT:",end=" ")
     for i in range(len(input_test_arrays)):
-        MergeSort.merge_sort(input_test_arrays[i])
+        merge_sort.sort(input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
     print("PASSED")
 
@@ -53,42 +53,52 @@ def test_bubblesort():
                     [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
     print("BUBBLE SORT:",end=" ")
     for i in range(len(input_test_arrays)):
-        BubbleSort.bubble_sort(input_test_arrays[i])
+        bubble_sort.sort(input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
     print("PASSED")
 
 def test_radixsort():
     input_test_arrays = [[1, 12, 9, 5, 6, 10],
-                    [-1, 12, 0, -5, 6, 10],
-                    [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
+                    [1, 121, 34, 46, 6, 10]]
+    
+    output_arrays = [ [1, 5, 6, 9, 10, 12],
+                [1, 6, 10, 34, 46, 121]]
+    
     print("RADIX SORT:",end=" ")
     for i in range(len(input_test_arrays)):
-        print("Before",input_test_arrays[i])
-        RadixSort.radix_sort(input_test_arrays[i])
-        print("After",input_test_arrays[i])
+        radix_sort.sort(input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
+    print("PASSED")
 
 def test_countingsort():
     input_test_arrays = [[1, 12, 9, 5, 6, 10],
-                    [-1, 12, 0, -5, 6, 10],
-                    [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
+                    [1, 121, 34, 46, 6, 10]]
+    
+    output_arrays = [ [1, 5, 6, 9, 10, 12],
+                [1, 6, 10, 34, 46, 121]]
+    
     print("COUNTING SORT:",end=" ")
     for i in range(len(input_test_arrays)):
         print("Before",input_test_arrays[i])
-        CountingSort.countingSort(input_test_arrays[i])
+        counting_sort.sort(input_test_arrays[i])
         print("After",input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
+    print("PASSED")
 
 def test_bucketsort():
-    input_test_arrays = [[1, 12, 9, 5, 6, 10],
-                    [-1, 12, 0, -5, 6, 10],
-                    [29, 99, 27, -1, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]]
+    input_test_arrays = [[1, 12, 9, 5, 6, 10, 11 ,12 ,13,14,15],
+                    [1, 121, 34, 46, 6, 10]]
+    
+    output_arrays = [ [1, 5, 6, 9, 10, 11, 12, 13, 14, 15],
+                [1, 6, 10, 34, 46, 121]]
+    
     print("BUCKET SORT:",end=" ")
     for i in range(len(input_test_arrays)):
         print("Before",input_test_arrays[i])
-        BucketSort.bucket_sort(input_test_arrays[i])
+        bucket_sort.sort(input_test_arrays[i])
         print("After",input_test_arrays[i])
         assert input_test_arrays[i] == output_arrays[i]
+    print("PASSED")
 
 
 test_quicksort()
@@ -97,4 +107,4 @@ test_mergesort()
 test_bubblesort()
 test_radixsort()
 #test_countingsort()
-#test_bucketsort()
+test_bucketsort()
